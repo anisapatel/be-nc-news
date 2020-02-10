d9e193b6b2566d7ab9cea8bcd5b73a1dba4a7710
anisapatel <anisa_patel@live.co.uk>
2020-02-10 10:32:13 +0000

    ready to deploy

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./Routes/apiRouter");
+app.use(cors());
 app.use(express.json());
 const {
   handle404s,
--- listen.js
+++ listen.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-  console.log("listening...");
+  console.log(`CORS-enabled web server listening on ${PORT}`);
 });
