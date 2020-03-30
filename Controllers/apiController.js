2bb62c42597583111c47bf6cee2362a89eea1ec8
anisapatel <anisa_patel@live.co.uk>
2020-03-30 09:42:20 +0100

    fixed get endpoints

--- Controllers/apiController.js
+++ Controllers/apiController.js
@@ -1,5 +1,4 @@
 const { fetchAllEndpoints } = require("../endpoints.json");
-const { fetchToken } = require("../Models/usersModel");
 
 exports.getAllEndpoints = (req, res, next) => {
   res.status(200).send(fetchAllEndpoints);
